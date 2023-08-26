b37d9b81a1b9cda635ee36e473c08c977f25d54f
AlexeyIvashchenko <iwashenko-lexa@mail.ru>
2023-08-26 22:04:53 +0300

    upload the files

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,34 @@
--- SQL-команды для создания таблиц
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(100) NOT NULL,
+	employee_id int NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100)
+);
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(100) PRIMARY KEY,
+    company_name varchar(100) UNIQUE NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+-- Добавляем внешние ключи
+#не мог разобраться как связать столбцы, выдавало ошибку про уникальность ключей,
+#поэтому нашёл этот способ в интернете
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id);
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employee FOREIGN KEY (employee_id) REFERENCES employees(employee_id);-- SQL-команды для создания таблиц
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,37 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+with psycopg2.connect(host="localhost", database="postgres", user="postgres", password="sygm6DK?") as conn:
+    with conn.cursor() as cur:
+        # Заполнение таблицы employees
+        with open('north_data/employees_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (row[0], row[1], row[2], row[3], row[4], row[5])
+                )
+
+        # Заполнение таблицы customers
+        with open('north_data/customers_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                    (row[0], row[1], row[2])
+                )
+
+        # Заполнение таблицы orders
+        with open('north_data/orders_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                    (row[0], row[1], row[2], row[3], row[4])
+                )
+conn.close()
